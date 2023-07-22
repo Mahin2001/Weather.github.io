9951062304dfb1ec51fee0ca9d4361a0b163f748
Tanhid Bin Hasan Mahin <111452498+Mahin2001@users.noreply.github.com>
2023-07-22 18:51:49 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,58 @@
+const apikey = "0285a7b43ebc25f5ee4a0c0c9c0d3a93";
+
+const weatherData = document.getElementById("Weather-data");
+
+const cityInput = document.getElementById("city-input");
+
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (event)=>{
+event.preventDefault();
+
+const cityValue = cityInput.value;
+console.log(cityValue);
+
+getWeatherData(cityValue);
+
+})
+
+async function getWeatherData(cityValue){
+
+    try{
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${apikey}&units=metric`)
+
+        if(!response.ok){
+            throw new Error("Network response was not ok")
+        }
+
+        const data = await response.json();
+
+        // console.log(data);
+
+        const temperature = Math.round(data.main.temp)
+
+        const description = data.weather[0].description
+
+        const details = [
+            `Feels like: ${Math.round(data.main.feel_like)}`,
+            `Humidity: ${data.main.humidity}%`,
+            `Wind speed: ${data.wind.speed}m/s`,
+        ]
+
+        weatherData.querySelector(".temperature").textContent = `${temperature}℃`;
+
+        weatherData.querySelector(".description").textContent = description;
+
+        weatherData.querySelector(".details").innerHTML = details.map((detail)=>`<div>${detail}</div>`).join("");
+        
+    } catch(error){
+
+
+        weatherData.querySelector(".temperature").textContent = "";
+
+        weatherData.querySelector(".description").textContent = "An error occured, please try again leter!";
+
+        weatherData.querySelector(".details").innerHTML = "";
+        
+    }
+}
